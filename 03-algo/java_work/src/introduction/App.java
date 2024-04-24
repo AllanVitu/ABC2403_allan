26b098a4a6d92064a024c1df50c48ae25ce90bee
Stitsheo <166002323+AllanVitu@users.noreply.github.com>
2024-04-24 15:01:04 +0200

    commit pour exemple java

--- 03-algo/java_work/src/introduction/App.java
+++ 03-algo/java_work/src/introduction/App.java
@@ -5,7 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		
 		// Affiche "Hello world" dans la console
-		System.out.println("Julien le plus beau des bg !");
+		System.out.println("Hello World !");
+		
 		
 		// VARIABLES
 		String prenom; // Déclaration du'une variable ""prenom de type " chaine de caracteres
@@ -13,13 +14,27 @@ public class App {
 		int age; // Déclaration d'une variable "age" de type "Nombre entier" (valeur scolaire)
 		// (plage de valeur d' INT - (2 147 000 000 à +2 147 000 000)
 		
+		
 		long soldeBancaire; // Déclaration d'une varaible "soldeBancaire" de type "Nombre entier 64 bits" (valeur scolaire)
+		
 		
 		float tailleEnMetres; // Déclaration d'une variable "tailleEnMetres" de type "Nombre réel 64 bits" (valeur scolaire)
 		
+		
 		double tailleEnMetresPourLesTresGrands; // Déclaration d'une variable "tailleEnMetresPourLesTresGrands" de type "Nombre réel 64 bits" (valeur scolaire)
 		
+		
 		Boolean estMajeur; //Déclaration d'une variable "estMajeur" de type "Booléen" (valeur scolaire)
+		
+		
+		
+		prenom = "Allan"; // Affectation d'une valeur à la variable "prenom"
+		
+		System.out.println("Salut " + prenom);
+		
+		prenom = "toto"; // modifcation de la valeur de la variable "prenom"
+		
+		System.out.println("Bonjour " + prenom);
 		
 	}
 
