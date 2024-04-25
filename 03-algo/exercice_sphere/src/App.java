1c25c8d17263daa86f93ad47bccc967db5bc8f47
Stitsheo <166002323+AllanVitu@users.noreply.github.com>
2024-04-25 12:52:42 +0200

    commit du désespoir

---
+++ 03-algo/exercice_sphere/src/App.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+import java.lang.Math;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Exercie Sphere");
+		
+		double rayon;
+		double volume;
+		double aire;
+		Scanner sc;
+		
+		sc = new Scanner(System.in);
+		
+		aire = (4.0 * Math.PI * (Math.pow(rayon,  2 )));
+		
+		volume = (4.0 * Math.PI *(Math.pow(rayon, 3 )));
+		
+	}
+
+}
---
+++ 03-algo/exercice_sphere/src/README.md
@@ -0,0 +1,23 @@
+# Exercice 1.4 : Calcul de l’aire et du volume d’une sphère
+Lire le rayon R d’une sphère puis calculer et afficher :
+
+Son aire = 4 π R²
+Son volume = (4 π R³)/3
+
+
+**VARIABLES**
+
+```
+sphereAire 
+
+sphereVolume
+
+rayon
+```
+
+**TRAITEMENT**
+
+```
+Saisir sphereAire = 
+
+
