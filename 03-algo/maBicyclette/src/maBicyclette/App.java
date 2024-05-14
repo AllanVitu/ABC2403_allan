064cd300a8cae9dc2465c1d8a733cfb3f63fefa7
Stitsheo <166002323+AllanVitu@users.noreply.github.com>
2024-05-14 12:55:22 +0200

    comit bicyclette

--- 03-algo/maBicyclette/src/maBicyclette/App.java
+++ 03-algo/maBicyclette/src/maBicyclette/App.java
@@ -1,8 +1,31 @@
 package maBicyclette;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
+		
+		Scanner sc;
+		sc = new Scanner(System.in);
+		
+		Boolean beauTemps;
+		Boolean veloOk;
+		Boolean reparationsImmediate;
+		Boolean livrePossede;
+		Boolean livreDispoBibliotheque;
+		
+		beauTemps = sc.nextBoolean();
+		System.out.println("Je vais me ballader");
+		if (beauTemps == true){
+			
+		}
+		
+		veloOk = sc.nextBoolean();
+		System.out.println("J'irai me ballader à bicyclette");
+		if (veloOk == true) {
+			
+		}
 
 
 	}
